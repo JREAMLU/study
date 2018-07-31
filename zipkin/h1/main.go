51b3b0296bcce2c1514cbaf3ee9a130e92dfc52a
jream <ms.08.lu@gmail.com>
2018-07-31 18:48:02 +0800

    feat(h1):

--- zipkin/h1/main.go
+++ zipkin/h1/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"time"
-
-	"github.com/JREAMLU/j-kit/go-micro/trace/opentracing"
 	"github.com/JREAMLU/j-kit/go-micro/util"
 	"github.com/JREAMLU/j-kit/http"
 	"github.com/JREAMLU/study/zipkin/h1/config"
@@ -11,42 +8,28 @@ import (
 	"github.com/JREAMLU/study/zipkin/h1/service"
 
 	"github.com/gin-gonic/gin"
-	micro "github.com/micro/go-micro"
-	clientGrpc "github.com/micro/go-plugins/client/grpc"
-	registerConsul "github.com/micro/go-plugins/registry/consul"
-	transportGrpc "github.com/micro/go-plugins/transport/grpc"
-	// microClient "github.com/micro/go-plugins/client/grpc"
 )
 
 func main() {
-	t, err := util.NewTrace("go.http.srv.h1", "v1", []string{"10.200.119.128:9092", "10.200.119.129:9092", "10.200.119.130:9092"}, "web_log_get")
-	if err != nil {
-		panic(err)
-	}
+}
 
-	conf, err := config.LoadConfig()
-	if err != nil {
-		panic(err)
-	}
+// RunHTTPService run http service
+func RunHTTPService(conf *config.HelloConfig) {
+	ms, t := util.NewHTTPService(conf.Config)
 
-	ms := micro.NewService(
-		micro.Client(clientGrpc.NewClient()),
-		micro.Registry(registerConsul.NewRegistry()),
-		micro.Transport(transportGrpc.NewTransport()),
-		micro.WrapClient(opentracing.NewClientWrapper(t)),
-		micro.WrapHandler(opentracing.NewHandlerWrapper(t)),
-	)
-	ms.Init(
-		micro.RegisterTTL(1*time.Second),
-		micro.RegisterInterval(1*time.Second),
-	)
+	// init micro client
 	service.InitMicroClient(ms.Client())
-	// service.InitMicroClient(microClient.NewClient())
+
+	// init http client
 	service.InitHTTPClient(t)
 
 	g := gin.New()
-	g.Use(gin.Recovery(), gin.Logger(), http.HandlerHTTPRequestGin(t, "go.http.srv.h1"))
+	g.Use(
+		gin.Recovery(),
+		gin.Logger(),
+		http.HandlerHTTPRequestGin(t, conf.Service.Name),
+	)
 
 	g = router.GetRouters(g, conf)
-	g.Run(":8001")
+	g.Run(conf.Web.URL)
 }
