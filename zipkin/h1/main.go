01ad2d3624ef6e540cec8fdbe336c09ad2b06407
jream <ms.08.lu@gmail.com>
2018-08-01 16:01:01 +0800

    feat(http):

--- zipkin/h1/main.go
+++ zipkin/h1/main.go
@@ -1,34 +1,31 @@
 package main
 
 import (
-	"github.com/JREAMLU/j-kit/go-micro/util"
 	"github.com/JREAMLU/j-kit/http"
 	"github.com/JREAMLU/study/zipkin/h1/config"
 	"github.com/JREAMLU/study/zipkin/h1/router"
 	"github.com/JREAMLU/study/zipkin/h1/service"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// load config
+	conf, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	RunHTTPService(conf)
 }
 
 // RunHTTPService run http service
 func RunHTTPService(conf *config.HelloConfig) {
-	ms, t := util.NewHTTPService(conf.Config)
+	ms, g, t := http.NewHTTPService(conf.Config)
 
 	// init micro client
 	service.InitMicroClient(ms.Client())
 
 	// init http client
 	service.InitHTTPClient(t)
-
-	g := gin.New()
-	g.Use(
-		gin.Recovery(),
-		gin.Logger(),
-		http.HandlerHTTPRequestGin(t, conf.Service.Name),
-	)
 
 	g = router.GetRouters(g, conf)
 	g.Run(conf.Web.URL)
