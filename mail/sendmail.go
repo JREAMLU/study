921c2656c0db6a819726058317fc11e41265316d
jream <ms.08.lu@gmail.com>
2019-01-02 18:31:14 +0800

    feat(mail):

--- mail/sendmail.go
+++ mail/sendmail.go
@@ -1,34 +1,60 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
+	"github.com/matcornic/hermes"
 	"gopkg.in/gomail.v2"
 )
 
-var list []struct {
-	Name    string
-	Address string
-}
-
 func main() {
-	d := gomail.NewDialer("smtp.example.com", 587, "user", "123456")
-	s, err := d.Dial()
+	fromAccount := "账号@qq.com"
+	password := "密码"
+	address := "发送的邮箱"
+
+	d := gomail.NewDialer("smtp.qq.com", 587, fromAccount, password)
+	// d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	m := gomail.NewMessage()
+	// QQ邮箱 From 必须和发送的邮箱保持一致
+	m.SetHeader("From", fromAccount)
+	// m.SetHeader("From", "no-reply@xxx.cn")
+	m.SetHeader("To", address)
+	// m.SetAddressHeader("Cc", "", "dan")
+	m.SetHeader("Subject", "Newsletter #1")
+
+	h := hermes.Hermes{}
+
+	email := hermes.Email{
+		Body: hermes.Body{
+			FreeMarkdown: `
+> _Hermes_ service will shutdown the **1st August 2017** for maintenance operations. 
+
+Services will be unavailable based on the following schedule:
+
+| Services | Downtime |
+| :------:| :-----------: |
+| Service A | 2AM to 3AM |
+| Service B | 4AM to 5AM |
+| Service C | 5AM to 6AM |
+
+---
+
+Feel free to contact us for any question regarding this matter at [support@hermes-example.com](mailto:support@hermes-example.com) or in our [Gitter](https://gitter.im/)
+
+`,
+		},
+	}
+
+	body, err := h.GenerateHTML(email)
 	if err != nil {
 		panic(err)
 	}
 
-	m := gomail.NewMessage()
-	for _, r := range list {
-		m.SetHeader("From", "no-reply@plu.cn")
-		m.SetAddressHeader("To", r.Address, r.Name)
-		m.SetHeader("Subject", "Newsletter #1")
-		m.SetBody("text/html", fmt.Sprintf("Hello %s!", r.Name))
+	m.SetBody("text/html", body)
 
-		if err := gomail.Send(s, m); err != nil {
-			log.Printf("Could not send email to %q: %v", r.Address, err)
-		}
-		m.Reset()
+	if err := d.DialAndSend(m); err != nil {
+		log.Printf("Could not send email to %q: %v", address, err)
 	}
+	m.Reset()
 }
